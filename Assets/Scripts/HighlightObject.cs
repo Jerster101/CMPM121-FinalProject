3ed8ba76d85546b0253b47961ea217396097a2f6
Noah <102700523+nrhart@users.noreply.github.com>
2022-12-05 12:46:06 -0800

    Collecting pages

---
+++ Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+public class HighlightObject : MonoBehaviour
+{
+  private Color initialColor;
+  public bool noEmissionAtStart = true;
+  public Color highlightColor = Color.red;
+  public Color mousedownColor = Color.green;
+  public static int PageCount = 0;
+
+  public Material skyboxOne;
+  public Material skyboxTwo;
+  private Light[] lights;
+
+  private bool mouseon = false;
+  private Renderer myRenderer;
+
+  void Start() {
+    RenderSettings.skybox = skyboxOne;
+    Debug.Log("Page Count = " + PageCount);
+    myRenderer = GetComponent<Renderer>();
+    if (noEmissionAtStart)
+    initialColor = Color.black;
+    else
+    initialColor = myRenderer.material.GetColor("_EmissionColor");
+  }
+
+  void OnMouseEnter(){
+    mouseon = true;
+    myRenderer.material.SetColor("_EmissionColor", highlightColor);
+  }
+
+  void OnMouseExit(){
+    mouseon = false;
+    myRenderer.material.SetColor("_EmissionColor",initialColor);
+  }
+
+  void OnMouseDown(){
+    myRenderer.material.SetColor("_EmissionColor", mousedownColor);
+    Destroy(gameObject, 0.1f);
+    PageCount += 1;
+    Debug.Log("Page Count = " + PageCount);
+    if (PageCount == 2)
+    {
+        RenderSettings.skybox = skyboxTwo;
+        lights = FindObjectsOfType(typeof(Light)) as Light[];
+         foreach(Light light in lights)
+         {
+             light.intensity = 0.5f;
+         }
+    }
+  }
+
+  void OnMouseUp(){
+    if (mouseon)
+    myRenderer.material.SetColor("_EmissionColor", highlightColor);
+    else
+    myRenderer.material.SetColor("_EmissionColor", initialColor);
+  }
+}
---
+++ Assets/Scripts/PressKeyPickUpObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressKeyPickUpObject : MonoBehaviour
+{
+    public GameObject Instruction;
+    public GameObject ThisTrigger;
+    public GameObject ObjectOnGround;
+    public bool Action = false;
+
+    void Start()
+    {
+        Instruction.SetActive(false);
+        ThisTrigger.SetActive(true);
+        ObjectOnGround.SetActive(true);
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+    }
+
+}
