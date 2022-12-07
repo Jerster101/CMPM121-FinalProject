b748b41fd78b4c4ea6b1e8080a804495f0f6b131
Jeremy Dahlberg <34037966+Jerster101@users.noreply.github.com>
2022-12-06 20:04:53 -0800

    fixed movement and collision

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -4,33 +4,25 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
+    const float SENSITIVITY = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
 
-  void Start()
-  {
-    Cursor.visible = false;
-    Cursor.lockState = CursorLockMode.Locked;
-  }
+    void Start()
+    {
+        playerBody = transform.parent.GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-  public float Sensitivity {
-		get { return sensitivity; }
-		set { sensitivity = value; }
-	}
-	[Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
-	[Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
-	[Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * SENSITIVITY * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * SENSITIVITY * Time.deltaTime;
 
-	Vector2 rotation = Vector2.zero;
-	const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
-	const string yAxis = "Mouse Y";
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-	void Update(){
-		rotation.x += Input.GetAxis(xAxis) * sensitivity;
-		rotation.y += Input.GetAxis(yAxis) * sensitivity;
-		rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
-		var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
-		var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
-
-		transform.localRotation = yQuat; //Quaternions seem to rotate more consistently than EulerAngles. Sensitivity seemed to change slightly at certain degrees using Euler. transform.localEulerAngles = new Vector3(-rotation.y, rotation.x, 0);
-        transform.parent.gameObject.transform.localRotation = xQuat;
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,37 +4,49 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public float speed = 0.03f;
-    //public float rotSpeed = 1000;
+    float x;
+    float z;
+    CharacterController cc;
+    const float SPEED = 5f;
+    const float SPRINTSPEED = 8f;
+    const float GRAVITY = -9.81f;
+    Vector3 velocity;
+    bool isGrounded;
 
+    Transform groundCheck;
+    const float GROUNDDISTANCE = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        groundCheck = gameObject.transform.GetChild(0);
+    }
 
     void Update()
-    {   
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, GROUNDDISTANCE, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed = 0.06f;
-        } else if (Input.GetKeyUp(KeyCode.LeftShift)){
-            speed = 0.03f;
+            cc.Move(move * SPRINTSPEED * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
+        else
         {
-            transform.Translate(0f, 0f, speed);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-speed, 0f, 0f);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed, 0f, 0f);
+            cc.Move(move * SPEED * Time.deltaTime);
         }
 
-        //transform.Rotate((0), (Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime), 0, Space.World);
-       
-     }
-
+        velocity.y += GRAVITY * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+    }
 }
