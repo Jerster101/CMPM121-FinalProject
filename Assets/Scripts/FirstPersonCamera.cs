5e5d53ad4d449f4ec1ac518ebcf272e3a4b2d517
Jeremy Dahlberg <34037966+Jerster101@users.noreply.github.com>
2022-12-07 12:47:16 -0800

    stuff

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -24,5 +24,6 @@ public class FirstPersonCamera : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+        Debug.Log(Input.GetAxis("Mouse Y"));
     }
 }
