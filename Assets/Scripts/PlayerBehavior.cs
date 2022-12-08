70bd8c1d91c63d797520d1eb1788d1fc7101be62
Jeremy Dahlberg <34037966+Jerster101@users.noreply.github.com>
2022-12-07 17:35:50 -0800

    lots of stuff

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -8,16 +8,18 @@ public class PlayerBehavior : MonoBehaviour
     const float SPEED = 5f;
     const float SPRINTSPEED = 8f;
     const float GRAVITY = -9.81f;
-    const int MAXSTAMINA = 100;
+    //const int MAXSTAMINA = 100;
 
     private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
     private Coroutine regen;
+    //private Coroutine sprint;
 
-    int currentStamina;
+    //bool sprinting = false;
+    //int currentStamina;
     float x;
     float z;
     CharacterController cc;
-    [SerializeField] Slider staminaBar;
+    //[SerializeField] Slider staminaBar;
     Vector3 velocity;
     bool isGrounded;
 
@@ -27,12 +29,14 @@ public class PlayerBehavior : MonoBehaviour
 
     void Start()
     {
+        //sprint = StartCoroutine(Sprinting());
         cc = GetComponent<CharacterController>();
         groundCheck = gameObject.transform.GetChild(0);
 
-        currentStamina = MAXSTAMINA;
-        staminaBar.maxValue = MAXSTAMINA;
-        staminaBar.value = MAXSTAMINA;
+        //currentStamina = MAXSTAMINA;
+        //staminaBar.maxValue = MAXSTAMINA;
+        //staminaBar.value = MAXSTAMINA;
+        //StartCoroutine(Sprinting());
     }
 
     void Update()
@@ -49,26 +53,31 @@ public class PlayerBehavior : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        
+        if (Input.GetKey(KeyCode.LeftShift) /*&& staminaBar.value >= 0*/)
         {
             cc.Move(move * SPRINTSPEED * Time.deltaTime);
+            //sprinting = true;
+
         }
         else
         {
             cc.Move(move * SPEED * Time.deltaTime);
+            //sprinting = false;
         }
 
         velocity.y += GRAVITY * Time.deltaTime;
         cc.Move(velocity * Time.deltaTime);
     }
 
-    public void UseStamina(int amount)
+   /* public void UseStamina(int amount)
     {
         if(currentStamina - amount >= 0)
         {
             currentStamina -= amount;
             staminaBar.value = currentStamina;
 
+            if (regen != null) StopCoroutine(regen);
 
             regen = StartCoroutine(RegenStamina());
         }
@@ -83,5 +92,20 @@ public class PlayerBehavior : MonoBehaviour
             staminaBar.value = currentStamina;
             yield return regenTick;
         }
+        regen = null;
     }
+
+    private IEnumerator Sprinting()
+    {
+
+        if (sprinting)
+        {
+
+            if (regen != null) StopCoroutine(regen);
+            currentStamina -= MAXSTAMINA / 100;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
+        else regen = StartCoroutine(RegenStamina());
+    }*/
 }
