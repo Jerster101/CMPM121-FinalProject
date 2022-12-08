1b85adcc388a4d3371e987d8735d79098c41f59e
Jeremy Dahlberg <34037966+Jerster101@users.noreply.github.com>
2022-12-07 16:36:43 -0800

    fog beginning, stamina bar beginning, title screen

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    float x;
-    float z;
-    CharacterController cc;
     const float SPEED = 5f;
     const float SPRINTSPEED = 8f;
     const float GRAVITY = -9.81f;
+    const int MAXSTAMINA = 100;
+
+    private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+    private Coroutine regen;
+
+    int currentStamina;
+    float x;
+    float z;
+    CharacterController cc;
+    [SerializeField] Slider staminaBar;
     Vector3 velocity;
     bool isGrounded;
 
@@ -21,6 +29,10 @@ public class PlayerBehavior : MonoBehaviour
     {
         cc = GetComponent<CharacterController>();
         groundCheck = gameObject.transform.GetChild(0);
+
+        currentStamina = MAXSTAMINA;
+        staminaBar.maxValue = MAXSTAMINA;
+        staminaBar.value = MAXSTAMINA;
     }
 
     void Update()
@@ -48,5 +60,28 @@ public class PlayerBehavior : MonoBehaviour
 
         velocity.y += GRAVITY * Time.deltaTime;
         cc.Move(velocity * Time.deltaTime);
+    }
+
+    public void UseStamina(int amount)
+    {
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+
+
+            regen = StartCoroutine(RegenStamina());
+        }
+    }
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(2);
+        while (currentStamina < MAXSTAMINA)
+        {
+            currentStamina += MAXSTAMINA / 100;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
     }
 }
